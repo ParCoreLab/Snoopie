cc3dc3a7d3e9361755db06529b2ba73add14fdd8
Mohammad Issa <missa18@ku.edu.tr>
2023-03-08 10:07:44 +0300

    update runme for nccl test

--- test-apps/nccl/runme.sh
+++ test-apps/nccl/runme.sh
@@ -2,10 +2,14 @@
 
 . ~/envs/cuda-11-nccl-nvshmem.sh
 
-# export NVSHMEM_NVTX=common
+# If more verbosity is needed, uncomment the following line:
 # export NCCL_DEBUG=info
 
 
-# TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/noop/noop.so" ./main2
-# TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/cudaops/cudaops.so" ./main2
-TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/mem_multigpu/mem_multigpu.so" ./main2
+# To test the overhead of a "noop" instrumentation":
+# TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/noop/noop.so" ./main
+
+# To list the cuda operations in the binary:
+# TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/cudaops/cudaops.so" ./main
+
+TOOL_VERBOSE=1 KERNEL_NAME="ncclKernel_SendRecv_RING_SIMPLE_Sum_int8_t" LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/mem_multigpu/mem_multigpu.so" ./main
