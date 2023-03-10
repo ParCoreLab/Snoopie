dec84e43c0a6a2678ea79ad0fef65b198b41f649
Mohammad Issa <missa18@ku.edu.tr>
2023-03-10 17:29:37 +0300

    add runme for diim

--- test-apps/diim/runme.sh
+++ test-apps/diim/runme.sh
@@ -2,4 +2,4 @@
 
 . ~/envs/cuda-11-nccl-nvshmem.sh
 
-LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/mem_multigpu/mem_multigpu.so" KERNEL_NAME="simple_kernel" ./simple-diim
+LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/mem_multigpu/mem_multigpu.so" KERNEL_NAME="simple_kernel" ./diim
