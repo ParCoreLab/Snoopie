7df8d4317758dfa811255fd457fa834cfb4b4ad2
Mohammad Kefah <missa18@ku.edu.tr>
2023-08-12 15:24:42 +0200

    clean and merge test folders

--- test-apps/diim/runme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. ~/envs/cuda-11-nccl-nvshmem.sh
-
-LD_PRELOAD="../../tools/mem_multigpu/mem_multigpu.so" KERNEL_NAME="simple_kernel" ./diim
--- test-apps/mult-kernels/runme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. ~/envs/cuda-11-nccl-nvshmem.sh
-
-LD_PRELOAD="$HOME/proj/nvbit/nvbit_release/tools/mem_multigpu/mem_multigpu.so" KERNEL_NAME="simple_kernel" ./simple-diim
