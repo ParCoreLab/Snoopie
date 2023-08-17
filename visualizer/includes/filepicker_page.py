9063fb9ddc50d174cb187b56c036109bba34a3cb
emrgncr <egencer20@ku.edu.tr>
2023-08-17 11:22:12 +0300

    change local variable names to prevent confusion with global variable names

--- visualizer/includes/filepicker_page.py
+++ visualizer/includes/filepicker_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from .filepath_handler import *
-from . import streamlit_globals
+from .streamlit_globals import *
 
 
 def can_continue():
@@ -13,30 +13,38 @@ def can_continue():
         return True
 
 
+_logfile = None
+_src_code_file = None
+_sampling_period = None
+_gpu_num = None
+
+
 def filepicker_page():
-    global accept_btn, logfile, src_code_file, _sampling_period, gpu_num
+    global _logfile, _src_code_file, _sampling_period, _gpu_num
 
     if can_continue():
         return False
 
-    logfile = st.file_uploader("Log File", accept_multiple_files=False)
-    src_code_file = st.file_uploader("Source Code File", accept_multiple_files=False)
-    gpu_num = st.number_input(
+    _logfile = st.file_uploader("Log File", accept_multiple_files=False)
+    _src_code_file = st.file_uploader("Source Code File", accept_multiple_files=False)
+    _gpu_num = st.number_input(
         "Number of GPU's", -1, 16, -1, help="Leave -1 for automatic detection"
     )
 
     _sampling_period = st.number_input(
-        "Sampling Period", 0, 100, streamlit_globals.sampling_period
+        "Sampling Period", 0, 100, 10
     )
     st.session_state.filepicker_button = st.button("Start")
 
-    if logfile != None and src_code_file != None:
-        st.session_state.logfile = file_from_upload_check(logfile)
-        st.session_state.logfile_name = logfile.name
-        st.session_state.src_code_file = file_from_upload(src_code_file)
+    if _logfile != None and _src_code_file != None:
+        st.session_state.logfile = file_from_upload_check(_logfile)
+        st.session_state.logfile_name = _logfile.name
+        st.session_state.src_code_file = file_from_upload(_src_code_file)
 
         st.session_state.sampling_period = _sampling_period
-        st.session_state.gpu_num = gpu_num
+        st.session_state.gpu_num = _gpu_num
 
         st.session_state.show_filepicker = False
-        streamlit_globals.setup_globals()
+        
+        setup_globals()
+        
