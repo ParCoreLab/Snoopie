7bbfca7b752904a03d7c6bca026ffb2758e734d5
Mohammad Kefah <missa18@ku.edu.tr>
2024-02-05 03:53:14 +0300

    reorder function declarations and add missing newline at end of file

--- src/mem_multigpu/object_info.h
+++ src/mem_multigpu/object_info.h
@@ -6,22 +6,19 @@
 #include <string>
 #include <cstdint>
 
-std::string get_object_var_name(uint64_t pc);
-
-std::string get_object_file_name(uint64_t pc);
-
-std::string get_object_func_name(uint64_t pc);
-
-uint32_t get_object_line_num(uint64_t pc);
-
-int get_object_device_id(uint64_t pc);
-
-void set_object_device_id(uint64_t pc, int dev_id);
-
-uint32_t get_object_data_type_size(uint64_t pc);
-
-void set_object_data_type_size(uint64_t pc, const uint32_t type_size);
+void initialize_object_table(int size);
 
 bool object_exists(uint64_t pc);
+int get_object_device_id(uint64_t pc);
+
+std::string get_object_file_name(uint64_t pc);
+std::string get_object_func_name(uint64_t pc);
+std::string get_object_var_name(uint64_t pc);
+
+uint32_t get_object_data_type_size(uint64_t pc);
+uint32_t get_object_line_num(uint64_t pc);
+
+void set_object_data_type_size(uint64_t pc, const uint32_t type_size);
+void set_object_device_id(uint64_t pc, int dev_id);
 
 #endif // OBJECT_INFO_H
